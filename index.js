7bbaf0f4a98ab375a03c650760e604b65e11561e
collangat@gmail.com <collangat@gmail.com>
2023-09-23 00:34:46 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser= `grace`;
+const welcomeMessage=`Welcome to Flatbook, ${currentUser} !`;
+//const excitedWelcomeMessage = `${welcomeMessage.toUpperCase}`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
